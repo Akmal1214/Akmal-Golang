aee842ffd2d0a40e04235fdd3f5ef9f170c4d32a
P1ogram <programpro1214@gmail.com>
2023-09-25 18:09:54 +0500

    скоро

--- main.go
+++ main.go
@@ -180,7 +180,7 @@ func main() {
     fmt.Println("Приложения успешно выполнилось")
 }*/
 
-package main 
+/*package main 
 
 import "fmt"
 
@@ -195,5 +195,144 @@ func main() {
 	 delete(users, "Аслан")
 
 	fmt.Println(users)
+}*/
+
+/*package main 
+
+import "fmt"
+
+type User struct {
+	name string
+	age int64
+	password string
 }
 
+func main() {
+	user := User{name:"Tokuchi", age:27, password: "pass"}
+	user.name = "Jhon"
+	fmt.Println(user.nameUser())
+}
+
+func (g User) nameUser() string{
+	return g.name
+}*/
+
+/*package main 
+
+import "fmt"
+
+type NumberStruct struct{ // создаём структуру 
+	num1 int 
+	num2 int
+}
+
+type Number interface { // создаём интерфейс 
+	Sum() int 
+	Multiply() int 
+	Division() float64
+	Substract() int
+}
+
+func (n NumberStruct) Sum() int{ // создаем метод для структуры, унаследуем метод интерфейса для структуры 
+	return n.num1 + n.num2
+}
+
+func main(){
+	var i NumberStruct // создаем переменную которая будет вместить наш интерфейс 
+	numbers := NumberStruct{15, 5} // создаем нашу структуру на основе чисел
+	i = numbers // в интерфейс мы помещаем нашу структуру 
+	fmt.Println(i.Sum()) 
+}*/
+
+/*package main 
+
+import "fmt"
+
+func main() {
+	for i := 0; i <= 10; i++{
+		if i == 8{
+			continue
+		}
+		fmt.Println(i)
+	}
+}*/
+
+/*package main
+
+import "fmt"
+
+func main() {
+	numbers := [3][5]int {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	fmt.Println(numbers[2][2])
+}*/
+
+/* ------------------ДОМАШНИЕ ЗАДАНИЯ---------------*/
+
+/*package main
+
+import (
+	"fmt"
+	"time"
+	"github.com/zhashkevych/scheduler"
+	"cache"
+)
+
+type Example struct {
+	Name string 
+	age int 
+}
+
+
+func main() {
+	newExample := &Example {
+		Name: "Akmal",
+		Age:  15,
+	}
+
+	c := cache.New(3*time.Minute, 5*time.Minute)
+
+	abcd, found := c.Get("abcd")
+
+	if found {
+		a:= abcd.([]Example)
+		a = append(a, *newExample)
+		c.Set("abcd", a, cache.NoExpiration)
+		fmt.Println("Ну короче, да")
+	}else {
+		c.Set("abcd", newExample, cache.NoExpiration)
+		fmt.Println("Ну короче, нет")
+	}
+
+	abcd, found = c.Get("abcd")
+	if found {
+		fmt.Println(abcd)
+	}
+}*/
+
+/*package main 
+
+import (
+	"fmt"
+	"github.com/jellydator/ttlcache/v2"
+)
+
+func main() {
+	cache := cache.New()
+
+	cache.Set("Akmal", 14)
+	userId := cache.Get("userId")
+
+	fmt.Println(userId)
+
+	cache.Delete("userId")
+
+	fmt.Println(userId)
+}*/
+
+package main
+
+import "fmt"
+
+func main() {
+fmt.Println("Hello world")
+}
